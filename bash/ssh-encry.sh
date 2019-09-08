0a0e246f7c4cc4853cb476f40530a148dc04d729
CaribouW <1071956678@qq.com>
2019-09-08 13:08:57 +0800

    travis

--- bash/ssh-encry.sh
+++ bash/ssh-encry.sh
@@ -1,6 +1,3 @@
-ssh-keygen -t rsa -b 4096 -C 'build@travis-ci.org' -f ./deploy_rsa
-travis encrypt-file deploy_rsa --add
-ssh-copy-id -i deploy_rsa.pub root@106.14.140.93
+travis login --auto
+travis encrypt-file ~/.ssh/id_rsa --add
 
-rm -f deploy_rsa deploy_rsa.pub
-git add deploy_rsa.enc .travis.yml
