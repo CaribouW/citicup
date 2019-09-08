4d61e66c2d341b8706077798422a7bc560c95592
CaribouW <1071956678@qq.com>
2019-09-09 07:10:47 +0800

    将全部的部署脚本放置到单一文件

--- bash/setup-deploy.sh
+++ bash/setup-deploy.sh
@@ -3,5 +3,7 @@ bash bash/install.sh
 # cp to the static
 cp -r frontend-admin/networker/* server/citicup-frontend/networker
 cp -r frontend-user/networker/* server/citicup-frontend/networker
+ssh root@106.14.140.93 StrictHostKeyChecking=no 'rm -rf citicup/citicup-frontend/networker/*'
+
 cd server && bash bash/local_setup.sh && cd ..
 ssh root@106.14.140.93 StrictHostKeyChecking=no 'cd citicup && bash setup.sh'
