76bbdd66850de131484ef062403b436a48adb226
CaribouW <1071956678@qq.com>
2019-09-09 11:44:33 +0800

    ls

--- bash/install.sh
+++ bash/install.sh
@@ -1,2 +1,2 @@
-cd frontend-admin && npm cache clean -f && npm install && npm run build && ls networker && cd ..
-cd frontend-user && npm cache clean -f && npm install && npm run build && ls networker && cd ..
+cd frontend-admin && npm cache clean -f && npm install && npm run build && cd ..
+cd frontend-user && npm cache clean -f && npm install && npm run build && cd ..
