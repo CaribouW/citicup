5566b0e6c6b485dc9c733c94d499f42313399ff7
CaribouW <1071956678@qq.com>
2019-09-09 15:39:25 +0800

    setup_cp

---
+++ bash/cp.sh
@@ -0,0 +1,1 @@
+cp -r frontend-user/networker/* server/citicup-frontend/networker && cp -r frontend-admin/networker/* server/citicup-frontend/networker
--- bash/setup-deploy.sh
+++ bash/setup-deploy.sh
@@ -1,8 +1,6 @@
 git submodule update --init --recursive --remote
 
 # cp to the static
-cp -r frontend-user/networker/* server/citicup-frontend/networker
-cp -r frontend-admin/networker/* server/citicup-frontend/networker
 ssh root@106.14.140.93 StrictHostKeyChecking=no 'rm -rf citicup/citicup-frontend/networker/*'
 
 cd server && bash bash/local_setup.sh
