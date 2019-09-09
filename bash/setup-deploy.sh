36962375ddabe5ceb81d4d9711e321f4db567ef6
CaribouW <1071956678@qq.com>
2019-09-09 13:45:57 +0800

    add_mkdir

--- bash/setup-deploy.sh
+++ bash/setup-deploy.sh
@@ -1,8 +1,8 @@
 git submodule update --init --recursive --remote
 
 # cp to the static
-mv frontend-user/networker/* server/citicup-frontend/networker
-mv frontend-admin/networker/* server/citicup-frontend/networker
+cp -r frontend-user/networker/* server/citicup-frontend/networker
+cp -r frontend-admin/networker/* server/citicup-frontend/networker
 ssh root@106.14.140.93 StrictHostKeyChecking=no 'rm -rf citicup/citicup-frontend/networker/*'
 
 cd server && bash bash/local_setup.sh
